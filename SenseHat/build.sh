a9c9c862f40d4da4e0dd24588add7c468edcc885
Carson Hall <51799808+HellishHexagon@users.noreply.github.com>
2019-06-13 16:27:16 -0400

    Move DLL to more convenient location for users

--- SenseHat/build.sh
+++ SenseHat/build.sh
@@ -5,5 +5,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 pushd "$DIR" 1>/dev/null 2>&1
 msbuild SenseHat.sln /t:Rebuild /p:Configuration=Release
 
+mv SenseHat/bin/Release/SenseHat.dll SenseHat.dll
+
 popd 1>/dev/null 2>&1
 trap 2
