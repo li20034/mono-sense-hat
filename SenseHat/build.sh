60881ded49284603cbf3f1b4ae8ca44c537a4351
Zonggao Li <li20034@gmail.com>
2019-06-13 17:11:26 -0400

    build.sh: fall back to xbuild if msbuild not found

--- SenseHat/build.sh
+++ SenseHat/build.sh
@@ -3,9 +3,18 @@ trap '' 2
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 pushd "$DIR" 1>/dev/null 2>&1
-msbuild SenseHat.sln /t:Rebuild /p:Configuration=Release
 
-cp SenseHat/bin/Release/SenseHat.dll SenseHat.dll
+if [[ -x "`command -v msbuild`" ]]; then
+    msbuild SenseHat.sln /t:Rebuild /p:Configuration=Release
+elif [[ -x "`command -v xbuild`" ]]; then
+    echo "install.sh: msbuild not found. falling back to deprecated xbuild"
+    xbuild SenseHat.sln /t:Rebuild /p:Configuration=Release
+else
+    echo "install.sh: can't find mono build tools. install mono-devel, msbuild"
+    exit 1
+fi
+
+mv SenseHat/bin/Release/SenseHat.dll SenseHat.dll
 
 popd 1>/dev/null 2>&1
 trap 2
